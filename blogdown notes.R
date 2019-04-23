5cc4280b17949a43cdd9d9a6a49651de5078ce0f
Jenni Lei <j.lei2821@gmail.com>
2019-04-22 20:24:38 -0400

    first commit

---
+++ blogdown notes.R
@@ -0,0 +1,7 @@
+#install.packages("blogdown")
+#blogdown::install_hugo()
+#blogdown::hugo_version()
+#blogdown::new_site()
+#blogdown::serve_site() - implement LiveReload so any changes are automatically saved when you edit your source files(Rmarkdown), can also preview website locally 
+#config.toml - make changes to global setting, can change URLs
+#blogdown::install_theme("nirocfz/https://github.com/slunsford/arabica")
